5abeccdbc6ab27a5f5b2a3a4db22fd9b51e00cb8
MK <manuelkoestercg@gmail.com>
2023-08-15 23:12:28 +0200

    started improvements for render settings window

--- src/QuiltiX/usd_render_settings.py
+++ src/QuiltiX/usd_render_settings.py
@@ -1,60 +1,101 @@
-from Qt.QtCore import Qt # type: ignore
-from Qt.QtWidgets import QDoubleSpinBox, QLineEdit, QFormLayout, QWidget, QCheckBox, QSpinBox  # type: ignore
+from Qt import QtWidgets, QtCore  # type: ignore
+from NodeGraphQt.custom_widgets.properties_bin import node_property_widgets
+from QuiltiX.constants import VALUE_DECIMALS
 
-from pxr.Usdviewq.stageView import StageView, UsdImagingGL
+from pxr.Usdviewq.stageView import UsdImagingGL  # type: ignore
 
-class RenderSettingsWidget(QWidget):
-
+# Inherit from _PropertiesList so the layouts & styling are the same
+class RenderSettingsWidget(node_property_widgets._PropertiesList):
     def __init__(self, stage_view, window_title="Render Settings"):
         super(RenderSettingsWidget, self).__init__()
-        self.setAttribute(Qt.WA_StyledBackground, True)
         self.stage_view = stage_view
-        self.layout = QFormLayout(self)
+
+        # Imitate node_property_widgets._PropertiesContainer
+        self._main_widget = QtWidgets.QWidget()
+        self._main_grid_layout = QtWidgets.QGridLayout()
+        self._main_grid_layout.setColumnStretch(1, 1)
+        self._main_grid_layout.setSpacing(6)
+
+        layout = QtWidgets.QVBoxLayout(self._main_widget)
+        layout.setAlignment(QtCore.Qt.AlignTop)
+        layout.addLayout(self._main_grid_layout)
+
+        self.insertRow(0)
+        self.setCellWidget(0, 0, self._main_widget)
 
     def on_renderer_changed(self):
         self._clear_widgets()
         self._populate_widgets()
 
     def _clear_widgets(self):
-        for i in reversed(range(self.layout.count())):
-            widgetToRemove = self.layout.itemAt(i).widget()
-            self.layout.removeWidget(widgetToRemove)
-            widgetToRemove.setParent(None)
+        for i in reversed(range(self._main_grid_layout.count())):
+            item_to_remove = self._main_grid_layout.itemAt(i)
+            if item_to_remove:
+                widget_to_remove = item_to_remove.widget()
+                if widget_to_remove:
+                    self._main_grid_layout.removeWidget(widget_to_remove)
+                    widget_to_remove.deleteLater()
 
     def _populate_widgets(self):
         settings = self.stage_view.GetRendererSettingsList()
 
-        for setting in settings:
+        label_flags = QtCore.Qt.AlignCenter | QtCore.Qt.AlignRight
+
+        for i, setting in enumerate(settings):
+            label = f"{str(setting.key)}: "
+            value = self.stage_view.GetRendererSetting(setting.key)
+            self._main_grid_layout.addWidget(QtWidgets.QLabel(label), i, 0, label_flags)
+
             if setting.type == UsdImagingGL.RendererSettingType.FLAG:
-                checkBox = QCheckBox(self)
-                checkBox.setChecked(self.stage_view.GetRendererSetting(setting.key))
-                checkBox.key = str(setting.key)
-                checkBox.defValue = setting.defValue
+                checkBox = QtWidgets.QCheckBox(self._main_widget)
+
+                checkBox.setChecked(value)
+
                 checkBox.toggled.connect(lambda v, setting=setting: self.stage_view.SetRendererSetting(setting.key, v))
-                self.layout.addRow(setting.name, checkBox)
+
+                self._main_grid_layout.addWidget(checkBox, i, 1)
+
             elif setting.type == UsdImagingGL.RendererSettingType.INT:
-                spinBox = QSpinBox(self)
-                spinBox.setMinimum(-2 ** 31)
-                spinBox.setMaximum(2 ** 31 - 1)
+                spinBox = QtWidgets.QSpinBox(self._main_widget)
+                spinBox.wheelEvent = lambda _: None
+
+                spinBox.setMinimum(-(2**31))
+                spinBox.setMaximum(2**31 - 1)
+
                 spinBox.setValue(self.stage_view.GetRendererSetting(setting.key))
-                spinBox.key = str(setting.key)
-                spinBox.defValue = setting.defValue
-                spinBox.valueChanged.connect(lambda v, setting=setting: self.stage_view.SetRendererSetting(setting.key, v))
-                self.layout.addRow(setting.name, spinBox)
+
+                spinBox.valueChanged.connect(
+                    lambda v, setting=setting: self.stage_view.SetRendererSetting(setting.key, v)
+                )
+
+                self._main_grid_layout.addWidget(QtWidgets.QLabel(label), i, 0, label_flags)
+                self._main_grid_layout.addWidget(spinBox, i, 1)
+
             elif setting.type == UsdImagingGL.RendererSettingType.FLOAT:
-                spinBox = QDoubleSpinBox(self)
-                spinBox.setDecimals(10)
-                spinBox.setMinimum(-2 ** 31)
-                spinBox.setMaximum(2 ** 31 - 1)
+                spinBox = QtWidgets.QDoubleSpinBox(self._main_widget)
+                spinBox.wheelEvent = lambda _: None
+
+                spinBox.setDecimals(VALUE_DECIMALS)
+                spinBox.setMinimum(-(2**31))
+                spinBox.setMaximum(2**31 - 1)
+
                 spinBox.setValue(self.stage_view.GetRendererSetting(setting.key))
-                spinBox.key = str(setting.key)
-                spinBox.defValue = setting.defValue
-                spinBox.valueChanged.connect(lambda v, setting=setting: self.stage_view.SetRendererSetting(setting.key, v))
-                self.layout.addRow(setting.name, spinBox)
+
+                spinBox.valueChanged.connect(
+                    lambda v, setting=setting: self.stage_view.SetRendererSetting(setting.key, v)
+                )
+
+                self._main_grid_layout.addWidget(spinBox, i, 1)
+
             elif setting.type == UsdImagingGL.RendererSettingType.STRING:
-                lineEdit = QLineEdit(self)
+                lineEdit = QtWidgets.QLineEdit(self._main_widget)
+
                 lineEdit.setText(self.stage_view.GetRendererSetting(setting.key))
-                lineEdit.key = str(setting.key)
-                lineEdit.defValue = setting.defValue
-                lineEdit.textChanged.connect(lambda v, setting=setting: self.stage_view.SetRendererSetting(setting.key, v))
-                self.layout.addRow(setting.name, lineEdit)
+
+                lineEdit.textChanged.connect(
+                    lambda v, setting=setting: self.stage_view.SetRendererSetting(setting.key, v)
+                )
+
+                self._main_grid_layout.addWidget(lineEdit, i, 1)
+
+        # FIXME: make sure the widget renders. somehow the widget or its contents only render after resizing??
